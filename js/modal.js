07b760901941225f65d9bc94a579c14695c1b921
FimeMan0830 <t107590013@ntut.org.tw>
2020-07-02 03:35:14 +0800

    FINISH

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,17 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("modalBtn");
 var span = document.getElementsByClassName("close")[0];
+firebase.auth().onAuthStateChanged(function(user){
+    if(user){
+        btn.innerHTML = user.email;
+        console.log(user);
+    }else{
+        btn.innerHTML = "會員註冊/登入";
+        console.log("not log in");
+    }
+})
 btn.onclick = function() {
+
     modal.style.display = "block";
 }
 span.onclick = function() {
@@ -17,42 +27,65 @@ var email = document.getElementById("email");
 var password = document.getElementById("password");
 var loginBtn = document.getElementById("loginBtn");
 var registerBtn = document.getElementById("registerBtn");
+var logOutBtn = document.getElementById("logOutBtn");
 var loginFail = document.getElementById("loginFail")
 loginBtn.addEventListener("click", function (e) {
     e.preventDefault();
-    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-    .then(() => {
-        var user = firebase.auth().currentUser;
-        if(user){
-            console.log("登入成功");
-            console.log(user);
-            //window.location.href='register.html'
-            modal.style.display = "none";
-        }
-    })
-    .catch((error) => {
-        console.log(error.message);
-        loginFail.innerHTML = "登入失敗";
-    });
+    var user = firebase.auth().currentUser;
+    if(user){
+        alert('您已經登入過了');
+        console.log("您已經登入過了");
+    }else{
+        firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+        .then(() => {
+            user = firebase.auth().currentUser;
+            if(user){
+                console.log("登入成功");
+                console.log(user);
+                alert('登入成功');
+                modal.style.display = "none";
+            }
+        })
+        .catch((error) => {
+            alert('登入失敗');
+            console.log(error.message);
+        });
+    }
 });
 registerBtn.addEventListener("click", function (e) {
     e.preventDefault();
     firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
     .then(() => {
         console.log("註冊成功");
+        alert('註冊成功');
         modal.style.display = "none";
     })
     .catch((error) => {
         console.log(error.message);
-        loginFail.innerHTML = "註冊失敗";
+        alert('註冊失敗');
     });
 });
-/*
-firebase.auth().onAuthStateChanged(function(user){
+logOutBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    var user = firebase.auth().currentUser;
     if(user){
-        modal.innerHTML = "登出";
+        firebase.auth().signOut()
+        .then(() => {
+        alert('登出成功');
+        console.log("登出成功");
+        modal.style.display = "none";
+    })
+    }else{
+        alert('請先登入');
+        console.log("請先登入");
     }
-    else{
-        modal.innerHTML = "會員註冊/登入";
-    }
-});*/
+});
+// firebase.auth().onAuthStateChanged(function(data){
+//     if(UserNow){
+//         btn.innerHTML = "登出";
+//         console.log(UserNow);
+//     }else{
+//         btn.innerHTML = "會員註冊/登入";
+//         console.log("not log in");
+//     }
+// })
